02fba22de2c1de6b8a2e8becdff0b0fae2021709
Titulus Desiderio <titulus.web@gmail.com>
2015-03-01 20:01:14 +0300

    first working version

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-require('nw.gui').Window.get().showDevTools(); //show console at start
+// require('nw.gui').Window.get().showDevTools(); //show console at start
 
 var credentials = require('./private.js'); //config include domain user and pass
 
 var ActiveDirectory = require('activedirectory');
-var ad = new ActiveDirectory(credentials.dn, credentials.dc, credentials.user, credentials.pass);
+var ad = new ActiveDirectory(credentials.dn, credentials.dc, credentials.user, credentials.pass, {attributes: {user: [ 'cn', 'telephonenumber', 'mail' ]}});
 
 var groupName = 'Employees';  // that how i call group with all real persons
 ad.getUsersForGroup(groupName, function(err, users) {
@@ -15,6 +15,24 @@ ad.getUsersForGroup(groupName, function(err, users) {
   if (! users) console.log('Group: ' + groupName + ' not found.');
   else {
     console.log(users);
-    sorter.init();
+    users2table(users);
+    // sorter.init();
   }
 });
+
+function users2table (users) {
+	tablehtml = '';
+	for (i in users) {
+		var user = users[i];
+		console.log(user);
+		tablehtml+='<tr>';
+		tablehtml+=	'<td>'+user.cn+'</td>'
+		tablehtml+=	'<td>'+user.telephoneNumber+'</td>'
+		tablehtml+=	'<td>'+user.mail+'</td>'
+		tablehtml+='</tr>'
+	}
+	console.log(tablehtml);
+	$('#table tbody').html(tablehtml);
+	sorter.init();
+
+}
