32e2ce9eb853217392d16b0d4c5e05b254e24276
Titulus Desiderio <titulus.web@gmail.com>
2015-03-01 18:44:14 +0300

    get All employees\

--- js/script.js
+++ js/script.js
@@ -5,3 +5,15 @@ var credentials = require('./private.js'); //config include domain user and pass
 var ActiveDirectory = require('activedirectory');
 var ad = new ActiveDirectory(credentials.dn, credentials.dc, credentials.user, credentials.pass);
 
+var groupName = 'Employees';  // that how i call group with all real persons
+ad.getUsersForGroup(groupName, function(err, users) {
+  if (err) {
+    console.log('ERROR: ' +JSON.stringify(err));
+    return;
+  }
+
+  if (! users) console.log('Group: ' + groupName + ' not found.');
+  else {
+    console.log(users);
+  }
+});
