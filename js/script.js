b3fd82b496d4d07d02f04024f095297a38617dfa
Titulus Desiderio <titulus.web@gmail.com>
2015-03-01 18:42:02 +0300

    add AD initiation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 require('nw.gui').Window.get().showDevTools(); //show console at start
 
 var credentials = require('./private.js'); //config include domain user and pass
+
+var ActiveDirectory = require('activedirectory');
+var ad = new ActiveDirectory(credentials.dn, credentials.dc, credentials.user, credentials.pass);
+
