acd0a0616ece670ca5f7bf23da672035c640042c
unknown <a.kuntsevich@UNIS000116.lan.unisneft.com>
2015-03-02 13:45:27 +0300

    disable debug

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-require('nw.gui').Window.get().showDevTools(); //show console at start
+// require('nw.gui').Window.get().showDevTools(); //show console at start
 require('nw.gui').Window.get().maximize(); //maximize windows on start
 
 var credentials = require('./private.js'); //config include domain user and pass
 
 var ActiveDirectory = require('activedirectory');
-var ad = new ActiveDirectory(credentials.dn, credentials.dc, credentials.user, credentials.pass, {attributes: {user: [ 'cn', 'telephonenumber', 'mail', 'department']}});
+var ad = new ActiveDirectory(credentials.dn, credentials.dc, credentials.user, credentials.pass, {attributes: {user: [ 'cn', 'telephonenumber', 'mail', 'department', 'mobile']}});
 
 var groupName = 'Employees';  // that how i call group with all real persons
 ad.getUsersForGroup(groupName, function(err, users) {
@@ -33,7 +33,7 @@ function users2table (users) {
 		// console.log(user);
 		tablehtml+='<tr>';
 		tablehtml+=	'<td>'+user.cn+'</td>'
-		tablehtml+=	'<td>'+((user.telephoneNumber)?user.telephoneNumber:'')+'</td>'
+		tablehtml+=	'<td>'+((user.telephoneNumber)?user.telephoneNumber:'')+((user.mobile)?('<br/>'+user.mobile):'')+'</td>'
 		tablehtml+=	'<td>'+((user.mail)?'<a href="mailto:'+user.mail+'">'+user.mail+'</a>':'')+'</td>'
 		tablehtml+=	'<td>'+((user.department)?user.department:'')+'</td>'
 		tablehtml+='</tr>'
