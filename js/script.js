fdce6f4ad9c31cff74510f958bd7becce1763868
Titulus Desiderio <titulus.web@gmail.com>
2015-03-02 07:23:49 +0300

    add department and disable pagination by default

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ require('nw.gui').Window.get().showDevTools(); //show console at start
 var credentials = require('./private.js'); //config include domain user and pass
 
 var ActiveDirectory = require('activedirectory');
-var ad = new ActiveDirectory(credentials.dn, credentials.dc, credentials.user, credentials.pass, {attributes: {user: [ 'cn', 'telephonenumber', 'mail' ]}});
+var ad = new ActiveDirectory(credentials.dn, credentials.dc, credentials.user, credentials.pass, {attributes: {user: [ 'cn', 'telephonenumber', 'mail', 'department']}});
 
 var groupName = 'Employees';  // that how i call group with all real persons
 ad.getUsersForGroup(groupName, function(err, users) {
@@ -29,6 +29,7 @@ function users2table (users) {
 		tablehtml+=	'<td>'+user.cn+'</td>'
 		tablehtml+=	'<td>'+user.telephoneNumber+'</td>'
 		tablehtml+=	'<td>'+user.mail+'</td>'
+		tablehtml+=	'<td>'+user.department+'</td>'
 		tablehtml+='</tr>'
 	}
 	// console.log(tablehtml);
