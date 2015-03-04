6765eb1ea7362766f11c5f11f0c91eedd285e900
unknown <a.kuntsevich@UNIS000116.lan.unisneft.com>
2015-03-04 11:10:17 +0300

    disable localstorage for debugging. try to add otherMobile

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-// require('nw.gui').Window.get().showDevTools(); //show console at start
+require('nw.gui').Window.get().showDevTools(); //show console at start
 require('nw.gui').Window.get().maximize(); //maximize windows on start
 
 var credentials = require('./private.js'); //config include domain user and pass
 
 var ActiveDirectory = require('activedirectory');
-var ad = new ActiveDirectory(credentials.dn, credentials.dc, credentials.user, credentials.pass, {attributes: {user: [ 'cn', 'telephonenumber', 'mail', 'department', 'mobile']}});
+var ad = new ActiveDirectory(credentials.dn, credentials.dc, credentials.user, credentials.pass, {attributes: {user: [ 'cn', 'telephonenumber', 'mail', 'department', 'mobile', 'otherMobile']}});
 
 var groupName = 'Employees';  // that how i call group with all real persons
 ad.getUsersForGroup(groupName, function(err, users) {
@@ -15,28 +15,35 @@ ad.getUsersForGroup(groupName, function(err, users) {
 
   if (! users) console.log('Group: ' + groupName + ' not found.');
   else {
-  	var localusers = localStorage.users;
-  	var ldapusers = JSON.stringify(users);
-    if (localusers != ldapusers) {
-    	localStorage.users = JSON.stringify(users);
-    	console.info('users changed');
-    } else {console.log('users didn\'t changed')};
-    users2table(JSON.parse(localStorage.users));
+  	// var localusers = localStorage.users;
+  	// var ldapusers = JSON.stringify(users);
+  	console.log(users);
+    // if (localusers != ldapusers) {
+    // 	localStorage.users = JSON.stringify(users);
+    // 	console.info('users changed');
+    // } else {console.log('users didn\'t changed')};
+    // users2table(JSON.parse(localStorage.users));
+    users2table(users)
   }
 });
 
-if (localStorage.users) users2table(JSON.parse(localStorage.users));
+// if (localStorage.users) users2table(JSON.parse(localStorage.users));
 function users2table (users) {
 	tablehtml = '';
 	for (i=0; i<users.length;i++) {
 		var user = users[i];
 		// console.log(user);
 		tablehtml+='<tr>';
-		tablehtml+=	'<td>'+user.cn+'</td>'
-		tablehtml+=	'<td>'+((user.telephoneNumber)?user.telephoneNumber:'')+((user.mobile)?('<br/>'+user.mobile):'')+'</td>'
-		tablehtml+=	'<td>'+((user.mail)?'<a href="mailto:'+user.mail+'">'+user.mail+'</a>':'')+'</td>'
-		tablehtml+=	'<td>'+((user.department)?user.department:'')+'</td>'
-		tablehtml+='</tr>'
+		tablehtml+=	'<td>'+user.cn+'</td>';
+		tablehtml+=	'<td>';
+			if (user.telephoneNumber) tablehtml+=user.telephoneNumber;
+			if (user.mobile) tablehtml+='<br/>'+user.mobile;
+			// for (otherMobile in user.otherMobile)
+			if (user.otherMobile) console.log(user.otherMobile)
+		tablehtml+= '</td>';
+		tablehtml+=	'<td>'+((user.mail)?'<a href="mailto:'+user.mail+'">'+user.mail+'</a>':'')+'</td>';
+		tablehtml+=	'<td>'+((user.department)?user.department:'')+'</td>';
+		tablehtml+='</tr>';
 	}
 	// console.log(tablehtml);
 	$('#table tbody').html(tablehtml);
