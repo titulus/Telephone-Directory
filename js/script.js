981582f519af16eb36daa260571129d8d46ea254
unknown <a.kuntsevich@UNIS000116.lan.unisneft.com>
2015-03-04 12:59:21 +0300

    stop debug

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-require('nw.gui').Window.get().showDevTools(); //show console at start
+// require('nw.gui').Window.get().showDevTools(); //show console at start
 require('nw.gui').Window.get().maximize(); //maximize windows on start
 
 var credentials = require('./private.js'); //config include domain user and pass
 
 var ActiveDirectory = require('activedirectory');
-var ad = new ActiveDirectory(credentials.dn, credentials.dc, credentials.user, credentials.pass, {attributes: {user: [ 'cn', 'mail', 'department', 'telephonenumber', 'otherTelephone', 'mobile', 'otherMobile', 'homePhone', 'facsimileTelephoneNumber']}});
+var ad = new ActiveDirectory(credentials.dn, credentials.dc, credentials.user, credentials.pass, {attributes: {user: [ 'cn', 'mail', 'department', 'title', 'telephonenumber', 'otherTelephone', 'mobile', 'otherMobile', 'homePhone', 'facsimileTelephoneNumber']}});
 
 var groupName = 'Employees';  // that how i call group with all real persons
 ad.getUsersForGroup(groupName, function(err, users) {
@@ -15,15 +15,15 @@ ad.getUsersForGroup(groupName, function(err, users) {
 
   if (! users) console.log('Group: ' + groupName + ' not found.');
   else {
-  	// var localusers = localStorage.users;
-  	// var ldapusers = JSON.stringify(users);
-  	console.log(users);
-    // if (localusers != ldapusers) {
-    // 	localStorage.users = JSON.stringify(users);
-    // 	console.info('users changed');
-    // } else {console.log('users didn\'t changed')};
-    // users2table(JSON.parse(localStorage.users));
-    users2table(users)
+  	var localusers = localStorage.users;
+  	var ldapusers = JSON.stringify(users);
+  	// console.log(users);
+    if (localusers != ldapusers) {
+    	localStorage.users = JSON.stringify(users);
+    	console.info('users changed');
+    } else {console.log('users didn\'t changed')};
+    users2table(JSON.parse(localStorage.users));
+    // users2table(users)
   }
 });
 
@@ -38,7 +38,7 @@ function users2table (users) {
 		tablehtml+=	'<td>';
 			if (user.telephoneNumber) tablehtml+='<i class="fa fa-phone"></i>'+user.telephoneNumber;
 			if (user.otherTelephone) {
-				console.log(typeof user.otherTelephone,user.otherTelephone)
+				// console.log(typeof user.otherTelephone,user.otherTelephone)
 				if (typeof user.otherTelephone == "string") {
 					tablehtml+='<br/>'+'<i class="fa fa-phone"></i>'+user.otherTelephone;
 				} else {
@@ -47,7 +47,7 @@ function users2table (users) {
 			}
 			if (user.mobile) tablehtml+='<br/>'+'<i class="fa fa-mobile fa-lg"></i>'+user.mobile;
 			if (user.otherMobile) {
-				console.log(typeof user.otherMobile,user.otherMobile)
+				// console.log(typeof user.otherMobile,user.otherMobile)
 				if (typeof user.otherMobile == "string") {
 					tablehtml+='<br/>'+'<i class="fa fa-mobile fa-lg"></i>'+user.otherMobile;
 				} else {
@@ -59,7 +59,7 @@ function users2table (users) {
 
 		tablehtml+= '</td>';
 		tablehtml+=	'<td>'+((user.mail)?'<a href="mailto:'+user.mail+'">'+user.mail+'</a>':'')+'</td>';
-		tablehtml+=	'<td>'+((user.department)?user.department:'')+'</td>';
+		tablehtml+=	'<td>'+((user.department)?user.department:'')+((user.title)?('</br>'+user.title):'')+'</td>';
 		tablehtml+='</tr>';
 	}
 	// console.log(tablehtml);
